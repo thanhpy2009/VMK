1b7b3a7c9afa0b8221575b7b99c91ac95e37f51c
thanhpy2009 <thanhpy2009@gmail.com>
2026-01-24 17:10:49 +0700

    Cập nhật nội dung mới cho thư mục src-full

--- src-full/CMakeFiles/org.fcitx.Fcitx5.Addon.Bamboo.metainfo.xml.in-fmt.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/org.fcitx.Fcitx5.Addon.Bamboo.metainfo.xml.in-fmt"
-  "org.fcitx.Fcitx5.Addon.Bamboo.metainfo.xml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/org.fcitx.Fcitx5.Addon.Bamboo.metainfo.xml.in-fmt.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src-full/Messages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gen_pot cxx:appdata:ui:desktop fcitx5-bamboo po .
